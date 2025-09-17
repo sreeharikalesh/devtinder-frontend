6af7318e893e3a3a7af5a6e46c27275ebf9aaee3
sreehari kalesh <sreeharikalesh@gmail.com>
2025-09-17 15:30:28 +0530

    added routing

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Outlet } from 'react-router'
+import Footer from './Footer'
+const Body = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Body
